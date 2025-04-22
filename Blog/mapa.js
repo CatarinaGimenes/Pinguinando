17d6efeaa6a833c0db55f5ad9198f48a895ee4f7
CatarinaGimenes <catarina.pires@sptech.school>
2025-04-22 00:23:24 -0300

    1- 3 páginas principais do blog + primeiro protótipo do mapa de redirecionamento

---
+++ Blog/mapa.js
@@ -0,0 +1,14 @@
+/*function abrirMapa() {
+    blogId.innerHTML += `
+    <div class="tela-inteira">
+        <div class="mapa-inteiro">
+            <img class="montanha" onclick="levarPagina()" src="../Blog/Fotos/montanha.png">
+            <img class="EPF" src="../Blog/Fotos/EstranhaParafernaliaFalante.png">
+            <img class="farol" src="../Blog/Fotos/farol.png">
+            <img class="catalogo" src="../Blog/Fotos/catalogo.png">
+            <img class="plaza" src="../Blog/Fotos/plaza.png">
+            <img class="dojo" src="../Blog/Fotos/dojo.png">
+        </div>
+    </div>
+    `
+}*/
