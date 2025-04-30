9a28d542af6c46dffa5b77f872814c6f565ff96d
CatarinaGimenes <catarina.pires@sptech.school>
2025-04-29 23:36:12 -0300

    Novas abas do blog + protótipo do minigame

--- Blog/mapa.js
+++ Blog/mapa.js
@@ -1,14 +1,19 @@
-/*function abrirMapa() {
+function abrirMapa() {
     blogId.innerHTML += `
     <div class="tela-inteira">
-        <div class="mapa-inteiro">
-            <img class="montanha" onclick="levarPagina()" src="../Blog/Fotos/montanha.png">
-            <img class="EPF" src="../Blog/Fotos/EstranhaParafernaliaFalante.png">
-            <img class="farol" src="../Blog/Fotos/farol.png">
-            <img class="catalogo" src="../Blog/Fotos/catalogo.png">
-            <img class="plaza" src="../Blog/Fotos/plaza.png">
-            <img class="dojo" src="../Blog/Fotos/dojo.png">
+            <div class="mapa-inteiro">
+                <img onclick="redirecionarIndex()" class="montanha" src="Fotos/montanha.png">
+                <img class="EPF" src="Fotos/EstranhaParafernaliaFalante.png">
+                <img onclick="redirecionarPersonagens()" class="farol" src="Fotos/farol.png">
+                <img class="catalogo" src="Fotos/catalogo.png">
+                <img onclick="redirecionarLinhaTempo()" class="plaza" src="Fotos/plaza.png">
+                <img onclick="redirecionarCARDJITSU()" class="dojo" src="Fotos/dojo.png">
+                <img onclick="fechar()" class="xis" src="Fotos/Inventory_image-metade.png">
+            </div>
         </div>
-    </div>
-    `
-}*/
+        `
+}
+
+function fechar() {
+    blogId.innerHTML = ""
+}
---
+++ Blog/mapaPersonagens.js
@@ -0,0 +1,63 @@
+function redirecionarIndex() {
+    window.location.href = "Index.html"
+}
+
+function redirecionarLinhaTempo() {
+    window.location.href = "LinhadoTempo.html"
+}
+
+function redirecionarPersonagens() {
+    window.location.href = "Personagens.html"
+}
+
+function redirecionarROCKHOPPER() {
+    window.location.href = "rockhopper.html"
+}
+
+function redirecionarTIA_ARCTIC() {
+    window.location.href = "tia_arctic.html"
+}
+
+function redirecionarCADENCE() {
+    window.location.href = "cadence.html"
+}
+
+function redirecionarSENSEI() {
+    window.location.href = "sensei.html"
+}
+
+function redirecionarROOKIE() {
+    window.location.href = "rookie.html"
+}
+
+function redirecionarHERBERT() {
+    window.location.href = "herbert.html"
+}
+
+function redirecionarKLUTZY() {
+    window.location.href = "klutzy.html"
+}
+
+function redirecionarPENGUINBAND() {
+    window.location.href = "penguinband.html"
+}
+
+function redirecionarAGENTETP() {
+    window.location.href = "agenteTP.html"
+}
+
+function redirecionarGAMMAGAL() {
+    window.location.href = "gammagal.html"
+}
+
+function redirecionarSHADOWGUY() {
+    window.location.href = "shadowguy.html"
+}
+
+function redirecionarDOT() {
+    window.location.href = "dot.html"
+}
+
+function voltar() {
+    window.location.href = "Personagens.html"
+}
